5c95f9d91e98efa2752ccda79cc88c3376a56d04
ukpono-j <ukponoakpan270@gmail.com>
2022-02-11 16:54:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='Main-container'>
+      <div className='header'>
+        <div className='mini'>
+          <div className='mini-bx'>
+            <a href='#' className='home-login'>Login</a>
+            <a href='#' className='home-join'>Join Free Now</a>
+            <div className='nav-search'>
+              SI
+            </div>
+          </div>
+        </div>
+        <div className='navbar'>
+          <div className='logo'>
+            Logo
+          </div>
+          <div className='ul-area'>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Swingers</a></li>
+            <li><a href='#'>Kink</a></li>
+            <li><a href='#'>Sex</a></li>
+            <li><a href='#'>Relationship</a></li>
+            <li><a href='#'>Media</a></li>
+            <li><a href='#'>Travel</a></li>
+            <li><a href='#'>More</a></li>
+            <li><a href='#'>SDC Shop</a></li>
+            <li><a href='#'>Advertise on SDC</a></li>
+
+          </div>
+        </div>
+      </div>
+
+      {/* =================== FIRST CONTAINER STARTS ============ */}
+      <div className='first-container'>
+        <div className='container_x'>
+          <div className='first-container_title'>
+            <h1>Erotic Dating for Swingers, Open-Minded Couples & Singles</h1>
+            <p>Welcome to SDC.com, the world’s largest lifestyle community to find hookups, threesomes, and moresomes with like-minded swingers and kinksters.</p>
+            <h4>21,576 MEMBERS ONLINE NOW!</h4>
+            <div className='fd'>
+              <a href='#' className='signup_bx_1'>Signup free today and hookup tonight</a>
+            </div>
+          </div>
+        </div>
+        <div className='head_video_bx'>
+          <div className="head_video"></div>
+        </div>
+      </div>
+      {/* ============================= SECOND CONTAINER =================== */}
+      <div className='second_container'>
+        <div className='second_bx'>
+          <div className='bx_2'></div>
+          <div className='bx_3'></div>
+        </div>
+        {/* ============== S=second -1 ============= */}
+        <div className='second_1'>
+          <div>
+            <div className='second1'>
+            </div>
+            <div className='second2 m-d'>
+              <h4>American Sex Podcast Episode 42: Midori....</h4>
+              <p>Midori has changed countless sex lives for the better worldwide, and might just change yours</p>
+            </div>
+          </div>
+          <div>
+            <div className='second3'> </div>
+            <div className='second2 m-d'>
+              <h4>American Sex Podcast Episode 42: Midori....</h4>
+              <p>Want A More Satisfying Sex Life? Try This!</p>
+            </div>
+          </div>
+          <div>
+            <div className='second5'> </div>
+            <div className='second2 m-d'>
+              <h4>American Sex Podcast Episode 42: Midori....</h4>
+              <p>Midori has changed countless sex lives for the better worldwide, and might just change yours</p>
+            </div>
+          </div>
+        </div>
+
+        {/* ===================== Second-2 */}
+        <div className='second_1'>
+          <div>
+            <div className='second7'>
+            </div>
+            <div className='second2 m-d'>
+              <h4>American Sex Podcast Episode 42: Midori....</h4>
+              <p>Midori has changed countless sex lives for the better worldwide, and might just change yours</p>
+            </div>
+          </div>
+          <div>
+            <div className='second8'> </div>
+            <div className='second2 m-d'>
+              <h4>Does he really want me to do this....</h4>
+              <p>When it comes to oral sex, some women just can't give enough. This couple takes her!</p>
+            </div>
+          </div>
+          <div>
+            <div className='second9'> 
+            {/* <img scr="" */}
+            </div>
+            <div className='second2 m-d'>
+              <h4>Meet Theg00dfellaz, the Ultimate Fantasy.</h4>
+              <p>Ladies, did you know that there is a group of good-looking and educated respectful single</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
